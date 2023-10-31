fc7cc1d45e3648b7b18562a486869558ab6e3168
Minus345 <106837932+Minus345@users.noreply.github.com>
2023-10-31 23:33:45 +0100

    update

--- main.py
+++ main.py
@@ -2,11 +2,26 @@ import os
 import signal
 import subprocess
 import time
+import atexit
 
+import psutil
 from flask import Flask, request
 
 app = Flask(__name__)
 print(os.getpid())
+
+global child_pid, proc
+
+proc = None
+
+
+def kills(pid):
+    '''Kills all process'''
+    parent = psutil.Process(pid)
+    for child in parent.children(recursive=True):
+        child.kill()
+    parent.kill()
+
 
 @app.get('/version')
 def getJavaVersion():
@@ -18,17 +33,33 @@ def getJavaVersion():
 
 @app.post('/start')
 def startNuvoLed():
-    print("start")
-    p = subprocess.Popen(['java', '-jar', 'nuvoled-1.0-SNAPSHOT-jar-with-dependencies.jar'],
-                         stdout=subprocess.PIPE,
-                         shell=True)
-    time.sleep(2)
-    print(p.pid)
-    os.kill(p.pid, signal.CTRL_C_EVENT)
-    return "start", 200
+    global proc, child_pid
+    proc = subprocess.Popen(["java", "-jar", "nuvoled-1.0-SNAPSHOT-jar-with-dependencies.jar"], stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+
+    child_pid = proc.pid
+    print(child_pid)
+
+    (output, error) = proc.communicate()
+    s = error + output
+    return str(s), 200
+
+    # return "started", 200
+
+
+@app.get('/status')  # ------- seperate loop --------------
+def getStatus():
+    if proc is None:
+        return "offline", 200
+    (output, error) = proc.communicate()
+    s = error + output
+    return str(s), 200
 
 
 @app.post('/stop')
 def stopNuvoLed():
-    #p.send_signal(signal.CTRL_C_EVENT)
-    return "stop", 200
+    print("terminate")
+    kills(child_pid)
+    (output, error) = proc.communicate()
+    s = error + output
+    return str(s), 200
